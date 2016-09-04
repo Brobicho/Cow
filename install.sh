37d477555f66c2b7eada1d3ee915cc5d1f49e286
Timothee Fuzeau <timothee-fuzeau@hotmail.fr>
2016-09-04 20:51:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh 
 
 touch ../.cow.cow
-mv .Cow ~/.cache
 echo "Quelle commande cowdifier ?"
 read cmd
 echo "la commande $cmd a ete modifier par un alias" > README.md
@@ -9,6 +8,7 @@ echo "alias $cmd=\"sh ~/.cache/.Cow/cow.sh\"" >> ~/.shrc
 echo "alias $cmd=\"sh ~/.cache/.Cow/cow.sh\"" >> ~/.bashrc
 echo "alias $cmd=\"sh ~/.cache/.Cow/cow.sh\"" >> ~/.zshrc
 alias $cmd="sh ~/.cache/.Cow/cow.sh"
+mv .Cow ~/.cache
 echo "$cmd a ete cowdifier !"
 sleep 2
 clear
