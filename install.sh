c3eee8cb8687842dfefb15947d971e4197c8dbe7
Timothee Fuzeau <timothee-fuzeau@hotmail.fr>
2016-09-07 03:44:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ echo "alias $cmd=\"sh ~/.Cow/cow.sh\"" >> ~/.bashrc
 echo "alias $cmd=\"sh ~/.Cow/cow.sh\"" >> ~/.zshrc
 alias $cmd="sh ~/.Cow/cow.sh"
 echo "$cmd a ete cowdifier !"
-sleep 2
+source .shrc
 clear
